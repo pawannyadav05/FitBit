c088a501e6c9a947a14f5904b5c80427fff0d990
pawannyadav05 <kumarpawanyadav02@gmail.com>
2026-04-11 17:26:18 +0530

    Started Fronted portion of the Project implemented landingPage,LoginPage and SignupPage

---
+++ frontend/js/auth.js
@@ -0,0 +1,67 @@
+const API_URL = "http://localhost:5000/api/auth";
+
+async function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        alert("Please fill in all fields");
+        return;
+    }
+    try {
+        const res = await fetch(`${API_URL}/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            alert(data.msg || "Login failed");
+            return;
+        }
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("role", data.role);
+
+        if (data.role === "admin") {
+            window.location.href = "dashboard-admin.html";
+        } else if (data.role === "trainer") {
+            window.location.href = "dashboard-trainer.html";
+        } else {
+            window.location.href = "dashboard-user.html";
+        }
+    } catch (err) {
+        console.error(err);
+        alert("Server error. Please try again.");
+    }
+}
+
+async function signup() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (name === "" || email === "" || password === "") {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    try {
+        const res = await fetch(`${API_URL}/register`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, email, password })
+        });
+        const data = await res.json();
+
+        if (res.ok) {
+            alert("Account created successfully! Please login.");
+            window.location.href = "login.html";
+        } else {
+            alert(data.msg || "Registration failed");
+        }
+    } catch (err) {
+        console.error(err);
+        alert("Server error. Check if backend is running.");
+    }
+}
